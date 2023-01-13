7b64f8a46ac25d4ea62103b0343fdd040abbf2e3
sinkyoungdeok <tkagmd1@naver.com>
2023-01-14 01:01:45 +0900

    [PW-147] redis 세팅

---
+++ common/src/main/kotlin/com/pabi/common/redis/RedisRepository.kt
@@ -0,0 +1,25 @@
+package com.pabi.common.redis
+
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+
+@Component
+class RedisRepository(
+    val redisTemplate: RedisTemplate<String, String>
+) {
+
+    fun setValue(key: String, value: String, timeout: Long, unit: TimeUnit) {
+        val values = redisTemplate.opsForValue()
+        values.set(key, value, timeout, unit)
+    }
+
+    fun getValue(key: String): String? {
+        val values = redisTemplate.opsForValue()
+        return values.get(key)
+    }
+
+    fun delValue(key: String) {
+        redisTemplate.delete(key)
+    }
+}
