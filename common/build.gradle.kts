07fb38956d971f1593d9b8f24be9e77cb2302ac9
Bob Sin <tkagmd1@naver.com>
2023-01-13 19:22:05 +0900

    [PW-122] JWT 토큰 기본 설정 추가 (#10)

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -4,4 +4,12 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // jwt
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+    // security
+    implementation("org.springframework.boot:spring-boot-starter-security")
 }
---
+++ common/src/main/kotlin/com/pabi/common/jwt/TokenProvider.kt
@@ -0,0 +1,90 @@
+package com.pabi.common.jwt
+
+import io.jsonwebtoken.*
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
+import io.jsonwebtoken.security.SecurityException
+import mu.KotlinLogging
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.stereotype.Component
+import java.security.Key
+import java.util.*
+import java.util.stream.Collectors
+
+@Component
+class TokenProvider(
+    @param:Value("\${jwt.secret}") private val secret: String,
+    @Value("\${jwt.token-validity-in-seconds}") tokenValidityInMilliseconds: Long,
+    @Value("\${jwt.access-token-validity-in-seconds}") accessTokenValidityInSeconds: Long,
+    @Value("\${jwt.refresh-token-validity-in-seconds}") refreshTokenValidityInSeconds: Long
+) : InitializingBean {
+    private val tokenValidityInMilliseconds: Long
+    private val accessTokenValidityInMilliseconds: Long
+    private val refreshTokenValidityInMilliseconds: Long
+    private var key: Key? = null
+
+    init {
+        accessTokenValidityInMilliseconds = accessTokenValidityInSeconds * 1000
+        refreshTokenValidityInMilliseconds = refreshTokenValidityInSeconds * 1000
+        this.tokenValidityInMilliseconds = tokenValidityInMilliseconds
+    }
+
+    private val log = KotlinLogging.logger {}
+
+    override fun afterPropertiesSet() {
+        val keyBytes = Decoders.BASE64.decode(secret)
+        key = Keys.hmacShaKeyFor(keyBytes)
+    }
+
+    fun getAllClaimsFromToken(token: String?): Claims {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).body
+    }
+
+    fun getAuthentication(token: String?): Authentication {
+        val claims = Jwts
+            .parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .body
+        val authorities: Collection<GrantedAuthority> = Arrays.stream(
+            claims[AUTHORITIES_KEY].toString().split(",".toRegex()).dropLastWhile { it.isEmpty() }
+                .toTypedArray())
+            .map { role: String? ->
+                SimpleGrantedAuthority(
+                    role
+                )
+            }
+            .collect(Collectors.toList())
+        val principal = User(claims.subject, "", authorities)
+        return UsernamePasswordAuthenticationToken(principal, token, authorities)
+    }
+
+    fun validateToken(token: String?): Boolean {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token)
+            return true
+        } catch (e: SecurityException) {
+            log.info("잘못된 JWT 서명입니다.")
+        } catch (e: MalformedJwtException) {
+            log.info("잘못된 JWT 서명입니다.")
+        } catch (e: ExpiredJwtException) {
+            log.info("만료된 JWT 토큰입니다.")
+        } catch (e: UnsupportedJwtException) {
+            log.info("지원되지 않는 JWT 토큰입니다.")
+        } catch (e: IllegalArgumentException) {
+            log.info("JWT 토큰이 잘못되었습니다.")
+        }
+        return false
+    }
+
+    companion object {
+        private const val AUTHORITIES_KEY = "cp"
+    }
+}
