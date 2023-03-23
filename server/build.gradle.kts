f7fb1d4ebc9e1865f86514b3c36819b66cacdf1f
Bob Sin <tkagmd1@naver.com>
2023-03-24 01:19:45 +0900

    [PW-182] jacoco 세팅을 위해 구조 재개편 (#21)

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,12 +1,23 @@
+plugins {
+    kotlin("plugin.jpa") version "1.6.21"
+}
+
 dependencies {
     implementation(project(path = ":common", configuration = "default"))
-    
-    implementation(project(":user:infrastructure"))
-    implementation(project(":user:domain"))
 
     runtimeOnly("org.postgresql:postgresql")
     // mysql
     implementation("mysql:mysql-connector-java")
+
+    // persistence
+    implementation("javax.persistence:javax.persistence-api:2.2")
+
+    // jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.hibernate.validator:hibernate-validator:6.1.2.Final")
 }
 
 tasks.jar { enabled = false }
