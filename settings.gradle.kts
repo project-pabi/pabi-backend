4096a2b290dabe8997a049b77f8a56b5163957ba
sinkyoungdeok <tkagmd1@naver.com>
2023-03-24 01:19:11 +0900

    [PW-182] jacoco 세팅을 위해 구조 재개편

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,3 @@ rootProject.name = "pabi-backend"
 
 include("server")
 include("common")
-include(
-    "user",
-    "user:presentation",
-    "user:application",
-    "user:domain",
-    "user:infrastructure",
-)
